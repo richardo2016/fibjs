187dcfa4fefe0b374daee00cc7f4288883a3b5a8
richardo2016 <richardo2016@gmail.com>
2019-05-13 04:15:10 +0800

    ci, feat: use more jobs to build.

--- .travis/build.sh
+++ .travis/build.sh
@@ -4,9 +4,9 @@ set -ev
 
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
     echo "start docker build..."
-    DIR=`pwd`;sudo docker run -it -v ${DIR}:/home/ci fibjs/build-env:clang /bin/sh -c "cd /home/ci; sh build -j2 ${ARCH} ci"
+    DIR=`pwd`;sudo docker run -it -v ${DIR}:/home/ci fibjs/build-env:clang /bin/sh -c "cd /home/ci; sh build -j8 ${ARCH} ci"
 else
-    sh build -j2 ${ARCH} ci
+    sh build -j8 ${ARCH} ci
 fi
 
 exit 0;
