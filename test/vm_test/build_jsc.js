4f51718832dab383106911c4636d776fabc98462
richardo2016 <richardo2016@gmail.com>
2021-03-11 10:31:06 +0800

    test, feat: generate test/vm_test/*.jsc from ci.

--- test/vm_test/build_jsc.js
+++ test/vm_test/build_jsc.js
@@ -3,5 +3,7 @@ var fs = require('fs');
 var path = require('path');
 var util = require('util');
 
-fs.writeFile(path.join(__dirname, "jsc_test_" + os.arch() + ".jsc"),
-    util.compile("jsc_test.js", "module.exports = {a : 100};"));
+const jscContent = util.compile("jsc_test.js", "module.exports = {a : 100};");
+
+fs.writeFile(path.join(__dirname, `jsc_test_${os.arch()}.jsc`), jscContent);
+fs.writeFile(path.join(__dirname, `jsc_test_${os.arch()}-${os.platform()}.jsc`), jscContent);
