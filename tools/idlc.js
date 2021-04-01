ff0df29f1176d38e1dd2a49c21e0fd3122509b3b
richardo2016 <richardo2016@gmail.com>
2021-04-01 20:55:25 +0800

    [WIP] idl, feat: support generate fibjs idl IR from *.d.ts

--- tools/idlc.js
+++ tools/idlc.js
@@ -2,12 +2,15 @@ var path = require('path');
 var parser = require('./util/parser');
 var gen_code = require('./util/gen_code');
 var generator = require('./util/generator');
+var record_idljson = require('./util/record_idljson');
 
 var idlLang = process.env.FIBJS_IDL_LANG || 'zh-cn'
 var idlFolder = path.join(__dirname, `../idl/${idlLang}`);
 var baseCodeFolder = path.join(__dirname, "../fibjs/include/ifs/");
 
 var defs = parser(idlFolder);
+
+record_idljson(defs);
 
 // generate back
 generator(defs, 'zh-CN', 'zh-cn');
